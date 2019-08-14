a7ed6691db0852a34a3a8c9ed61c64552c576d88
KotyJ <54086009+KotyJ@users.noreply.github.com>
2019-08-13 20:58:37 -0400

    Hewwo UwU

--- app/src/main/java/com/example/dmtoolsv001/MainActivity.java
+++ app/src/main/java/com/example/dmtoolsv001/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //pls help UwU Okiedokie
+        //hieeeeee
     }
 }
